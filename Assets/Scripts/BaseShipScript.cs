8472afa7125904116f0ecab089cb85ca45bb8d97
Beesses <br1ck56733@gmail.com>
2019-08-26 17:58:01 +0300

    Make some visual and base preparations

---
+++ Assets/Scripts/BaseShipScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EdgeCollider2D))]
+//[RequireComponent(typeof())]
+public abstract class BaseShipScript : MonoBehaviour
+{
+    [SerializeField] public float CurrentHealth;
+    [SerializeField] private float MaxHealth;
+    [SerializeField] private float Damage;
+    [SerializeField] private float Speed;
+    public abstract void Attack();
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StarsScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsScript : MonoBehaviour
+{
+    Rigidbody2D RB;
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        RB.AddForce(new Vector2(0, -1) * 1, ForceMode2D.Impulse);
+    }
+    void Update()
+    {
+        if(gameObject.transform.position.y < -9.81)
+        {
+            transform.position = new Vector2(transform.position.x, 9.99f);
+        }
+    }
+}
